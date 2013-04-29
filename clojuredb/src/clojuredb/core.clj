9749056b76bee0a5dd20b42304fae3c562696d19
John Madison <john.c.madison@gmail.com>
2013-04-28 20:16:43 -0500

    Ability to assign skills to people and projects

--- clojuredb/src/clojuredb/core.clj
+++ clojuredb/src/clojuredb/core.clj
@@ -1,4 +1,4 @@
-(ns x
+(ns user
   (:require [clojure.java.jdbc :as sql])
 )
 
@@ -9,12 +9,76 @@
          :password "letmein"
          })
 
+(defn cleanup []
+
+(sql/with-connection db
+  (sql/drop-table :person)
+  (sql/drop-table :project)
+  (sql/drop-table :skill)
+  (sql/drop-table :person_skill)
+  (sql/drop-table :project_skill)
+  )
+)
+
+(defn list-people []
+  (sql/with-connection db
+    (sql/with-query-results rows
+      ["select person_id, person_name from person"]
+      (println rows))))
+
+(defn insert-person [name]
+  (sql/insert! db :person [:person_name] [name])
+)
+
+(defn insert-project [name]
+  (sql/insert! db :project [:project_name] [name])
+)
+
+(defn insert-skill [name]
+  (sql/insert! db :skill [:skill_name] [name])
+  )
+
+(defn assign-skill-to-person [db person-name skill-name years-experience]
+  (sql/execute! db
+    [(str "insert into person_skill (person_id, skill_id, years_experience) values ("
+          "(select person_id from person where person_name = ?),"
+          "(select skill_id from skill where skill_name = ?),?)")
+          person-name skill-name years-experience]
+
+    )
+  )
+
+(defn assign-skill-to-project [db person-name skill-name years-experience]
+  (sql/execute! db
+    [(str "insert into project_skill (project_id, skill_id, years_experience) values ("
+       "(select project_id from project where project_name = ?),"
+       "(select skill_id from skill where skill_name = ?),?)")
+     person-name skill-name years-experience]
+
+    )
+  )
+
+
+(defn addData [db]
+  (sql/with-connection db
+    (insert-person "Rich")
+    (insert-person "Stuart")
+    (insert-project "Datomic")
+    (insert-skill "Thinking")
+    (insert-skill "Writing")
+    )
+
+    (assign-skill-to-person db "Rich" "Thinking" 10)
+    (assign-skill-to-person db "Stuart" "Writing" 5)
+    (assign-skill-to-project db "Datomic" "Thinking" 4)
+
+  )
 
 (defn run []
   (sql/with-connection db
-  (sql/create-table :person
-     [:person_id "int" "auto_increment" "primary key"]
-     [:person_name "varchar(100)"]))
+    (sql/create-table :person
+      [:person_id "int" "auto_increment" "primary key"]
+      [:person_name "varchar(100)"]))
 
   (sql/with-connection db
     (sql/create-table :project
@@ -42,28 +106,6 @@
       [:years_experience "int"]
       )
     )
-  )
 
-(defn cleanup []
-
-(sql/with-connection db
-  (sql/drop-table :person)
-  (sql/drop-table :project)
-  (sql/drop-table :skill)
-  (sql/drop-table :person_skill)
-  (sql/drop-table :project_skill)
-  )
-)
-
-
-
-(defn list-people []
-  (sql/with-connection db
-    (sql/with-query-results rows
-      ["select person_id, person_name from person"]
-      (println rows))))
-
-(defn insert-person [name]
-  (sql/insert! db :person [:person_name] [name])
-
+    (addData db)
   )
